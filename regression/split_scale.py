8739c97da24e817d694ae13b3981219cf01aff6e
Misty <ms.misty.garcia@gmail.com>
2019-10-10 15:05:30 -0500

    it works

--- regression/split_scale.py
+++ regression/split_scale.py
@@ -25,29 +25,52 @@ def split_my_data():
     X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=train_pct, random_state=123)
     return X_train, X_test, y_train, y_test
 
-# standard_scaler()
-def standard_scaler():
-    train, test, y_train, x_test = split_my_data()
 
-    scaler = StandardScaler(copy=True, with_mean=True, with_std=True).fit(train)
-
+def transform_scaler():  
     train_scaled = pd.DataFrame(scaler.transform(train), columns=train.columns.values).set_index([train.index.values])
     test_scaled = pd.DataFrame(scaler.transform(test), columns=test.columns.values).set_index([test.index.values])
-
     return train_scaled, test_scaled
+
+# standard_scaler()
+def standard_scaler():
+    X_train, X_test, y_train, y_test = split_my_data()
+
+    scaler = StandardScaler(copy=True, with_mean=True, with_std=True).fit(X_train)
+
+    # X_train_scaled, X_test_scaled = transform_scaler()
+
+    X_train_scaled = pd.DataFrame(scaler.transform(X_train), columns=X_train.columns.values).set_index([X_train.index.values])
+    X_test_scaled = pd.DataFrame(scaler.transform(X_test), columns=X_test.columns.values).set_index([X_test.index.values])
+
+    y_train = pd.DataFrame(y_train)
+    y_test = pd.DataFrame(y_test)
+
+    scaler = StandardScaler(copy=True, with_mean=True, with_std=True).fit(y_train)
+
+    y_train_scaled = pd.DataFrame(scaler.transform(y_train), columns=y_train.columns.values).set_index([y_train.index.values])
+    y_test_scaled = pd.DataFrame(scaler.transform(y_test), columns=y_test.columns.values).set_index([y_test.index.values])
+
+    return X_train_scaled, X_test_scaled, y_train_scaled, y_test_scaled
+
+standard_scaler()
 
 # scale_inverse()
 def scale_inverse():
-    train_scaled, test_scaled = standard_scaler()
+    train_scaled, test_scaled, scaler = standard_scaler()
+
+    train_unscaled = pd.DataFrame(scaler.inverse_transform(train_scaled), columns=train_scaled.columns.values).set_index([train_scaled.index.values])
+    test_unscaled = pd.DataFrame(scaler.inverse_transform(test_scaled), columns=test_scaled.columns.values).set_index([test_scaled.index.values])
+
 
     train_unscaled = pd.DataFrame(scaler.inverse_transform(train_scaled), columns=train_scaled.columns.values).set_index([train.index.values])
+
     test_unscaled = pd.DataFrame(scaler.inverse_transform(test_scaled), columns=test_scaled.columns.values).set_index([test.index.values])
 
     return train_unscaled, test_unscaled
 
 # uniform_scaler()
 def uniform_scaler():
-    train, test, y_train, x_test = split_my_data()
+    train, test, y_train, y_test = split_my_data()
 
     scaler = QuantileTransformer(n_quantiles=100, output_distribution='uniform', random_state=123, copy=True).fit(train)
 
@@ -58,7 +81,7 @@ def uniform_scaler():
 
 # gaussian_scaler()
 def gaussian_scaler():
-    train, test, y_train, x_test = split_my_data()
+    train, test, y_train, y_test = split_my_data()
 
     scaler = PowerTransformer(method='yeo-johnson', standardize=False, copy=True).fit(train)
     
@@ -69,7 +92,7 @@ def gaussian_scaler():
 
 # min_max_scaler()
 def min_max_scaler():
-    train, test, y_train, x_test = split_my_data()
+    train, test, y_train, y_test = split_my_data()
     
     scaler = MinMaxScaler(copy=True, feature_range=(0,1)).fit(train)
 
@@ -80,7 +103,7 @@ def min_max_scaler():
 
 # iqr_robust_scaler()
 def iqr_robust_scaler():
-    train, test, y_train, x_test = split_my_data()
+    train, test, y_train, y_test = split_my_data()
 
     scaler = RobustScaler(quantile_range=(25.0,75.0), copy=True, with_centering=True, with_scaling=True).fit(train)
 

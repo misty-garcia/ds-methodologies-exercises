b4a8cffdf9a035ed29bd51ad29082883972e5645
Misty <ms.misty.garcia@gmail.com>
2019-11-04 17:09:20 -0600

    working through exercises

--- clustering/prep.py
+++ clustering/prep.py
@@ -53,3 +53,30 @@ def handle_missing_values(df, prop_required_column, prop_required_row):
 def fill_missing_values(df, fill_value):
     df.fillna(fill_value)
     return df
+
+def get_upper_outliers(s, k):
+    '''
+    Given a series and a cutoff value, k, returns the upper outliers for the
+    series.
+
+    The values returned will be either 0 (if the point is not an outlier), or a
+    number that indicates how far away from the upper bound the observation is.
+    '''
+    q1, q3 = s.quantile([.25, .75])
+    iqr = q3 - q1
+    upper_bound = q3 + k * iqr
+    return s.apply(lambda x: max([x - upper_bound, 0]))
+
+def add_upper_outlier_columns(df, k):
+    '''
+    Add a column with the suffix _outliers for all the numeric columns
+    in the given dataframe.
+    '''
+    # outlier_cols = {col + '_outliers': get_upper_outliers(df[col], k)
+    #                 for col in df.select_dtypes('number')}
+    # return df.assign(**outlier_cols)
+
+    for col in df.select_dtypes('number'):
+        df[col + '_outliers'] = get_upper_outliers(df[col], k)
+
+    return df
--- clustering/wrangle_mall.py
+++ clustering/wrangle_mall.py
@@ -5,3 +5,8 @@ import acquire
 
 def get_mall():
     return acquire.get_data("SELECT * FROM customers", "mall_customers")
+
+def wrangle():
+    df = get_mall()
+    df.set_index("customer_id", inplace=True)
+    return df
